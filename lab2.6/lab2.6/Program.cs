4f76cd0f9337d0a960cb821d649feb435f1d7564
4321iva <162304549+4321iva@users.noreply.github.com>
2024-09-30 10:23:03 +0300

    commit

---
+++ lab2.6/lab2.6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введіть вік: ");
+int age = int.Parse(Console.ReadLine());
+
+if (age >= 12 && age <= 18)
+{
+    Console.WriteLine("Ти підліток");
+}
+else
+{
+    Console.WriteLine("Ти не підліток");
+}
+
